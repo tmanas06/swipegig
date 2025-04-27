7c0723fb8549e740942a8991f573612b1b05c449
Gayathri P <p.gayathri.ch@gmail.com>
2025-04-27 14:19:56 +0530

    contract updation

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -1,8 +1,14 @@
-// Create src/utils/ipfs.ts
-export const convertIPFSURL = (url: string) => {
-    if (url.startsWith('ipfs://')) {
-      return `https://gateway.pinata.cloud/ipfs/${url.replace('ipfs://', '')}`;
-    }
-    return url;
-  };
+// src/utils/ipfs.ts
+export const convertIPFSURL = (input: string) => {
+  // Use your dedicated Pinata gateway from environment variables
+  const gateway = import.meta.env.VITE_PINATA_GATEWAY;
   
+  // Extract CID regardless of input format
+  let cid = input;
+  if (input.startsWith('ipfs://')) {
+    cid = input.replace('ipfs://', '');
+  }
+  
+  // Always use your dedicated gateway
+  return `https://${gateway}/ipfs/${cid}`;
+};
